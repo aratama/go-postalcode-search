542d62bcc75e295ba458d334700b0b191e9f910a
aratama <16192627+aratama@users.noreply.github.com>
2022-07-16 05:07:55 +0900

    warnings

--- katakana.go
+++ katakana.go
@@ -7,22 +7,21 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
+// https://www.serendip.ws/archives/6307
 var kanaConv = unicode.SpecialCase{
-	// ひらがなをカタカナに変換
 	unicode.CaseRange{
-		0x3041, // Lo: ぁ
-		0x3093, // Hi: ん
-		[unicode.MaxCase]rune{
+		Lo: 0x3041, // Lo: ぁ
+		Hi: 0x3093, // Hi: ん
+		Delta: [unicode.MaxCase]rune{
 			0x30a1 - 0x3041, // UpperCase でカタカナに変換
 			0,               // LowerCase では変換しない
 			0x30a1 - 0x3041, // TitleCase でカタカナに変換
 		},
 	},
-	// カタカナをひらがなに変換
 	unicode.CaseRange{
-		0x30a1, // Lo: ァ
-		0x30f3, // Hi: ン
-		[unicode.MaxCase]rune{
+		Lo: 0x30a1, // Lo: ァ
+		Hi: 0x30f3, // Hi: ン
+		Delta: [unicode.MaxCase]rune{
 			0,               // UpperCase では変換しない
 			0x3041 - 0x30a1, // LowerCase でひらがなに変換
 			0,               // TitleCase では変換しない
